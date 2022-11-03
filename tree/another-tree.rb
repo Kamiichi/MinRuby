34bff514879e4578fa4c8b64e96eb7a1d459d33b
Kamiichi <paperone430@gmail.com>
2022-11-03 15:52:25 +0900

    another-tree.rbの追加

---
+++ tree/another-tree.rb
@@ -0,0 +1,12 @@
+# 深さ優先探索かな
+tree = ["節1", ["節2", ["葉A"], ["節3", ["葉B"], ["葉C"]]], ["葉D"]]
+
+def preorder(tree)
+    p(tree[0])
+    if tree[0].start_with?("節")
+        preorder(tree[1])
+        preorder(tree[2])
+    end
+end
+
+preorder(tree)
