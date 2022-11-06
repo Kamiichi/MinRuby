0b19bd267d5e01103176d97ffb25997f0fd2b3d4
Kamiichi <paperone430@gmail.com>
2022-11-07 02:10:04 +0900

    evaluate関数に%, **の機能を追加

--- calc/interp.rb
+++ calc/interp.rb
@@ -1,30 +1,45 @@
 require "minruby"
 
-def sum(tree)
-    p(tree)
-    if tree[0] == "lit"
-        tree[1]
-    else # この場合はtree[0] == "+"
-        left = sum(tree[1])
-        right = sum(tree[2])
-        p(left + right)
-        left + right
-    end
-end
+# def sum(tree)
+#     p(tree)
+#     if tree[0] == "lit"
+#         tree[1]
+#     else # この場合はtree[0] == "+"
+#         left = sum(tree[1])
+#         right = sum(tree[2])
+#         p(left + right)
+#         left + right
+#     end
+# end
 
 def evaluate(tree)
-    if tree[0] == "lit"
+    case tree[0]
+    when "lit"
         tree[1]
-    else
-        if tree[0] == "+"
-            left = evaluate(tree[1])
-            right = evaluate(tree[2])
-            left + right
-        else # この場合はtree[0] == "*"
-            left = evaluate(tree[1])
-            right = evaluate(tree[2])
-            left * right
-        end
+    when "+"
+        left = evaluate(tree[1])
+        right = evaluate(tree[2])
+        left + right
+    when "-"
+        left = evaluate(tree[1])
+        right = evaluate(tree[2])
+        left - right
+    when "*"
+        left = evaluate(tree[1])
+        right = evaluate(tree[2])
+        left * right
+    when "/"
+        left = evaluate(tree[1])
+        right = evaluate(tree[2])
+        left / right
+    when "%"
+        left = evaluate(tree[1])
+        right = evaluate(tree[2])
+        left % right
+    when "**"
+        left = evaluate(tree[1])
+        right = evaluate(tree[2])
+        left ** right
     end
 end
 
