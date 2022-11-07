302b9d74a842f1eeb113bd3e0500c6b649679a4b
Kamiichi <paperone430@gmail.com>
2022-11-08 02:29:06 +0900

    変数の代入と参照の追加

--- var/interp.rb
+++ var/interp.rb
@@ -1,27 +1,31 @@
 require "minruby"
 
-def evaluate(tree)
+def evaluate(tree, env)
     case tree[0]
     when "lit"
         tree[1]
     when "+"
-        evaluate(tree[1]) + evaluate(tree[2])
+        evaluate(tree[1], env) + evaluate(tree[2], env)
     when "-"
-        evaluate(tree[1]) - evaluate(tree[2])
+        evaluate(tree[1], env) - evaluate(tree[2], env)
     when "*"
-        evaluate(tree[1]) * evaluate(tree[2])
+        evaluate(tree[1], env) * evaluate(tree[2], env)
     when "/"
-        evaluate(tree[1]) / evaluate(tree[2])
+        evaluate(tree[1], env) / evaluate(tree[2], env)
     when "func_call"
-        p(evaluate(tree[2]))
+        p(evaluate(tree[2], env))
     when "stmts"
         i = 1
         last = nil
         while tree[i] != nil
-            last = evaluate(tree[i])
+            last = evaluate(tree[i], env)
             i = i + 1
         end
         last
+    when "var_assign"
+        env[tree[1]] = evaluate(tree[2], env)
+    when "var_ref"
+        env[tree[1]]
     end
 end
 
@@ -32,4 +36,5 @@ str = minruby_load()
 tree = minruby_parse(str)
 
 # 計算の木を実行(計算)する
-answer = evaluate(tree)
+env = {}
+evaluate(tree, env)
