b29001328efe36f5cef7ba978d83073e63703f0a
Kamiichi <paperone430@gmail.com>
2022-11-09 02:32:24 +0900

    interp.rbのcase文の順番を整理

--- if/interp.rb
+++ if/interp.rb
@@ -2,16 +2,6 @@ require "minruby"
 
 def evaluate(tree, env)
     case tree[0]
-    when "if"
-        if evaluate(tree[1], env)
-            evaluate(tree[2], env)
-        else
-            evaluate(tree[3], env)
-        end
-    when "while"
-        while evaluate(tree[1], env)
-            evaluate(tree[2], env)
-        end
     when "lit"
         tree[1]
     when "+"
@@ -43,6 +33,16 @@ def evaluate(tree, env)
         env[tree[1]] = evaluate(tree[2], env)
     when "var_ref"
         env[tree[1]]
+    when "if"
+        if evaluate(tree[1], env)
+            evaluate(tree[2], env)
+        else
+            evaluate(tree[3], env)
+        end
+    when "while"
+        while evaluate(tree[1], env)
+            evaluate(tree[2], env)
+        end
     else
         p("この演算子は使えないよ")
         pp(tree[0])
--- if/test.rb
+++ if/test.rb
@@ -1,5 +1,9 @@
-i = 0
-while i < 10
-    p(i)
-    i = i + 1
+case 42
+when 0
+    p(0)
+when 1
+    p(1)
+else
+    p(2)
 end
+    
