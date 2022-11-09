6bd028d2aeace98a45a896a69e286af6b30d0a45
Kamiichi <paperone430@gmail.com>
2022-11-10 01:27:18 +0900

    組み込み関数の実装,ただしminruby_callでRubyの組み込み関数の実装を呼んでいる

--- built-in/interp.rb
+++ built-in/interp.rb
@@ -1,56 +1,67 @@
 require "minruby"
 
-def evaluate(tree, env)
+def evaluate(tree, genv, lenv)
     case tree[0]
     when "lit"
         tree[1]
     when "+"
         # env["plus_count"] = env["plus_count"] + 1
-        evaluate(tree[1], env) + evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) + evaluate(tree[2], genv, lenv)
     when "-"
-        evaluate(tree[1], env) - evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) - evaluate(tree[2], genv, lenv)
     when "*"
-        evaluate(tree[1], env) * evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) * evaluate(tree[2], envgenv, lenv)
     when "/"
-        evaluate(tree[1], env) / evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) / evaluate(tree[2], genv, lenv)
     when "%"
-        evaluate(tree[1], env) % evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) % evaluate(tree[2], genv, lenv)
     when "**"
-        evaluate(tree[1], env) ** evaluate(tree[2], env)
+        evaluate(tree[1], env) ** evaluate(tree[2], genv, lenv)
     when "=="
-        evaluate(tree[1], env) == evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) == evaluate(tree[2], genv, lenv)
     when "<"
-        evaluate(tree[1], env) < evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) < evaluate(tree[2], genv, lenv)
     when ">"
-        evaluate(tree[1], env) > evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv) > evaluate(tree[2], genv, lenv)
     when "func_call"
-        p(evaluate(tree[2], env))
+        args = []
+        i = 0
+        while tree[i + 2]
+            args[i] = evaluate(tree[i + 2], genv, lenv)
+            i = i + 1
+        end
+        mhd = genv[tree[1]]
+        if mhd[0] == "builtin"
+            minruby_call(mhd[1], args)
+        else
+            # 埋める
+        end
     when "stmts"
         i = 1
         last = nil
         while tree[i] != nil
-            last = evaluate(tree[i], env)
+            last = evaluate(tree[i], genv, lenv)
             i = i + 1
         end
         last
     when "var_assign"
-        env[tree[1]] = evaluate(tree[2], env)
+        lenv[tree[1]] = evaluate(tree[2], genv, lenv)
     when "var_ref"
-        env[tree[1]]
+        lenv[tree[1]]
     when "if"
-        if evaluate(tree[1], env)
-            evaluate(tree[2], env)
+        if evaluate(tree[1], genv, lenv)
+            evaluate(tree[2], genv, lenv)
         else
-            evaluate(tree[3], env)
+            evaluate(tree[3], genv, lenv)
         end
     when "while"
-        while evaluate(tree[1], env)
-            evaluate(tree[2], env)
+        while evaluate(tree[1], genv, lenv)
+            evaluate(tree[2], genv, lenv)
         end
     when "while2"
-        evaluate(tree[1], env)
-        while evaluate(tree[1], env)
-            evaluate(tree[2], env)
+        evaluate(tree[1], genv, lenv)
+        while evaluate(tree[1], genv, lenv)
+            evaluate(tree[2], genv, lenv)
         end
     else
         p("この演算子は使えないよ")
@@ -65,5 +76,6 @@ str = minruby_load()
 tree = minruby_parse(str)
 
 # 計算の木を実行(計算)する
-env = {}
-evaluate(tree, env)
+genv = { "p" => ["builtin", "p"]}
+lenv = {}
+evaluate(tree, genv, lenv)
